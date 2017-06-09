db3ab40eb17a0b5f8c70f32097c24763d038f0c8
DanZhang6 <Bavolhaha@qq.com>
2017-06-09 09:08:03 +0800

    Add Anomaly Detection Algorithm

--- Anomaly Detection.m
+++ Anomaly Detection.m
@@ -1,8 +1,96 @@
-%% initial %%
-clear;
+%% Anomaly Detection Algorithm
+%  Discription:
+%  
+%     estimateGaussian.m
+%     selectThreshold.m
+%     cofiCostFunc.m
+%% Initialization
+clear ;
+close all;
 clc;
-load('Building Features.mat');%attention?Matrix data are column vector
-row=size(X,1);%Indicator
-column=size(X,2);%number of vector(experiment vector in different situation)
-row_r=280;%Original Data
-XX=1:280;
+%% ================== Part 1: Load Dataset And Visualize Example  ===================
+load('CC20.mat');
+%  Visualize the example dataset
+for i=281:377
+plot(X(i, :), X(i+1, :), 'bx');% In program exercise X matrix is row vector,for each row "plot(X(:, 1), X(:, 2), 'bx');"
+pause
+end
+%% ================== Part 2: Estimate the dataset statistics ===================
+%  For this exercise, we assume a Gaussian distribution for the dataset.
+%
+%  We first estimate the parameters of our assumed Gaussian distribution, 
+%  then compute the probabilities for each of the points and then visualize 
+%  both the overall distribution and where each of the points falls in 
+%  terms of that distribution.
+%
+fprintf('Visualizing Gaussian fit.\n\n');
+
+%  Estimate my and sigma2
+[mu sigma2] = estimateGaussian(X);
+
+%  Returns the density of the multivariate normal at each data point (row) 
+%  of X
+p = multivariateGaussian(X, mu, sigma2);
+
+%  Visualize the fit
+visualizeFit(X,  mu, sigma2);
+xlabel('Latency (ms)');
+ylabel('Throughput (mb/s)');
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+%% ================== Part 3: Find Outliers ===================
+%  Now you will find a good epsilon threshold using a cross-validation set
+%  probabilities given the estimated Gaussian distribution
+% 
+
+pval = multivariateGaussian(Xval, mu, sigma2);
+
+[epsilon F1] = selectThreshold(yval, pval);
+fprintf('Best epsilon found using cross-validation: %e\n', epsilon);
+fprintf('Best F1 on Cross Validation Set:  %f\n', F1);
+fprintf('   (you should see a value epsilon of about 8.99e-05)\n\n');
+
+%  Find the outliers in the training set and plot the
+outliers = find(p < epsilon);
+
+%  Draw a red circle around those outliers
+hold on
+plot(X(outliers, 1), X(outliers, 2), 'ro', 'LineWidth', 2, 'MarkerSize', 10);
+hold off
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+%% ================== Part 4: Multidimensional Outliers ===================
+%  We will now use the code from the previous part and apply it to a 
+%  harder problem in which more features describe each datapoint and only 
+%  some features indicate whether a point is an outlier.
+%
+
+%  Loads the second dataset. You should now have the
+%  variables X, Xval, yval in your environment
+load('ex8data2.mat');
+
+%  Apply the same steps to the larger dataset
+[mu sigma2] = estimateGaussian(X);
+
+%  Training set 
+p = multivariateGaussian(X, mu, sigma2);
+
+%  Cross-validation set
+pval = multivariateGaussian(Xval, mu, sigma2);
+
+%  Find the best threshold
+[epsilon F1] = selectThreshold(yval, pval);
+
+fprintf('Best epsilon found using cross-validation: %e\n', epsilon);
+fprintf('Best F1 on Cross Validation Set:  %f\n', F1);
+fprintf('# Outliers found: %d\n', sum(p < epsilon));
+fprintf('   (you should see a value epsilon of about 1.38e-18)\n\n');
+pause
+
+
+
+
